a3b1eb439b7461c76e79f878acf6e05acbabfcfa
Alexdelia <alexandre.delille.57@gmail.com>
2020-10-22 16:23:26 +0200

    update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 15:47:54 by adelille          #+#    #+#             */
-/*   Updated: 2020/10/20 15:21:14 by adelille         ###   ########.fr       */
+/*   Updated: 2020/10/22 16:20:15 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,20 @@ typedef struct	s_split_next
 	size_t len;
 }
 
-void    *ft_memset(void *s, int c, size_t len);
-void    *ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dest, const void *src, size_t n);
-void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
-void    *ft_memmove(void *dest, const void *src, size_t n);
-size_t  ft_strlen(const char *str);
+void	*ft_memset(void *s, int c, size_t len);
+void	*ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
