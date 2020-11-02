bbbd823710c8f833c2a9f7ec333a4790c361ec3f
Alexdelia <alexandre.delille.57@gmail.com>
2020-11-02 02:59:25 +0100

    auto update .c && .h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 15:47:54 by adelille          #+#    #+#             */
-/*   Updated: 2020/10/27 21:56:13 by adelille         ###   ########.fr       */
+/*   Updated: 2020/11/02 02:58:44 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,10 @@ char	*ft_strnstr(const char *s, const char *to_find, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
