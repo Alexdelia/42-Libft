beb3730298f2501f1be2b6be46a1ed503b767c6a
Alexandre DELILLE <adelille@e2r13p7.42.fr>
2020-11-18 09:31:39 +0100

    fixed ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,32 +6,25 @@
 /*   By: user42 <adelille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 13:31:45 by user42            #+#    #+#             */
-/*   Updated: 2020/11/17 19:09:03 by adelille         ###   ########.fr       */
+/*   Updated: 2020/11/18 09:29:35 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
 {
 	t_list	*first;
 	t_list	*new;
 
-	if (!f || !del)
+	if (!f || !lst)
 		return (NULL);
 	first = NULL;
 	while (lst)
 	{
 		if (!(new = ft_lstnew((*f)(lst->content))))
 		{
-			while (first)
-			{
-				new = first->next;
-				(*del)(first->content);
-				free(first);
-				first = new;
-			}
-			lst = NULL;
+			ft_lstclear(&first, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&first, new);
