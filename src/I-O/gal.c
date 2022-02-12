876b1b426b2530281d1088087cc1bfcb44d64105
Alexdelia <alexandre.delille.57@gmail.com>
2022-02-12 19:26:36 +0100

    fix

--- src/I-O/gal.c
+++ src/I-O/gal.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 14:50:56 by adelille          #+#    #+#             */
-/*   Updated: 2022/02/12 19:24:34 by adelille         ###   ########.fr       */
+/*   Updated: 2022/02/12 19:26:31 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ char	*gal(const int fd, size_t *size, const bool all)
 		return (gal_error(fd, output));
 	output[res] = '\0';
 	*size += res;
-	if (res == 0 || (all == true && res < GAL_INIT_BUFFER))
+	if (res == 0 || (all == false && res < GAL_INIT_BUFFER))
 		return (output);
 	return (gal_big(fd, output, size));
 }
