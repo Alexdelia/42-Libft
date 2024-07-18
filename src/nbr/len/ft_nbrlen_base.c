ba9279180545c8e94f119a0f9d04afb1160b0de0
adelille <adelille@student.42.fr>
2024-07-18 20:13:00 +0200

    norm

--- src/nbr/len/ft_nbrlen_base.c
+++ src/nbr/len/ft_nbrlen_base.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 20:07:52 by adelille          #+#    #+#             */
-/*   Updated: 2024/07/18 20:09:24 by adelille         ###   ########.fr       */
+/*   Updated: 2024/07/18 20:12:44 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 inline size_t	ft_nbrlen_base(ssize_t n, size_t base)
 {
 	if (n < 0)
-		return (1 + ft_unbrlen_base((size_t)-n, base));
+		return (1 + ft_unbrlen_base((size_t)(-n), base));
 	return (ft_unbrlen_base((size_t)n, base));
 }
