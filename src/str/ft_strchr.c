8291f2195203b89a9fbf7dc4588efeb022e6a8e6
adelille <adelille@student.42.fr>
2024-07-18 19:21:57 +0200

    `strchr`

--- src/str/ft_strchr.c
+++ src/str/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 16:06:48 by adelille          #+#    #+#             */
-/*   Updated: 2023/11/19 14:54:44 by adelille         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:21:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,11 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] == (char)c)
-			return ((char *)(s + i));
-		i++;
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
 	}
-	if (s[i] == (char)c)
-		return ((char *)(s + i));
-	return (NULL);
+	return (false);
 }
